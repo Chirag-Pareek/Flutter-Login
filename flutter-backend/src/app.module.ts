fdc20de378c27c2dd1c175c6bbc350597c448c8b
Chirag-Pareek <chiragpareek677@gmail.com>
2026-03-22 16:37:31 +0530

    aicontroller fix

--- flutter-backend/src/app.module.ts
+++ flutter-backend/src/app.module.ts
@@ -14,6 +14,7 @@ import { PrismaModule } from '../prisma/prisma.module';
 import { ScheduleModule } from '@nestjs/schedule';
 import { UsageCronService } from './usage/usage.cron.service';
 import { JwtAuthGuard } from './auth/jwt.guard';
+import { AiController } from './ai/ai.controller';
 
 @Module({
   imports: [
@@ -32,7 +33,7 @@ import { JwtAuthGuard } from './auth/jwt.guard';
     ]),
   ],
 
-  controllers: [AppController],
+  controllers: [AppController , AiController],
 
   providers: [
     AppService,
